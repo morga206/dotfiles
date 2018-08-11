166ec78a3a9f46b5620f92abf9b694d54ef16501
Andrew Morgan <morga206@cse.msu.edu>
2018-08-11 02:07:28 -0400

    lots of homepage tweakin'

--- chrome-new-tab/.chrome-new-tab/js/clock.js
+++ chrome-new-tab/.chrome-new-tab/js/clock.js
@@ -7,27 +7,3 @@ function updateClock(){
 
 updateClock();
 var intervalID = window.setInterval(updateClock, 10000);
-
-
-(function() {
-  var heights = [],
-    sections = document.querySelectorAll('section');
-  for (var i = 0; i < sections.length; i++)
-    heights.push(sections[i].clientHeight);
-  var maxHeight = Math.max.apply(null, heights);
-  document.getElementById('wrap').style.height = maxHeight+'px';
-
-  chrome.bookmarks.getTree(function(results) {
-    results[0].children.forEach(function(dir) {
-      if (dir.title && dir.title == 'Bookmarks Bar') {
-        var html = '';
-        dir.children.forEach(function(bookmark) {
-          html += '<li><a href="'+bookmark.url+'">'+bookmark.title+'</a></li>';
-        });
-        document.getElementById('bookmarks').innerHTML = html;
-        return;
-      }
-    });
-  });
-
-}());
